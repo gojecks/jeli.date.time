432240e831728e79a8fb3d2528874b2f83f7501b
Joseph Gojecks <kadiri.joseph@allianz.com>
2018-01-07 01:26:15 +0700

    file indenting

--- src/jDateTime.js
+++ src/jDateTime.js
@@ -1,69 +1,62 @@
+/*
+    ELI DateTime Plugins
+    created by Gojecks Joseph
+    can be injected to Project as a required Module
 
-    /*
-        ELI DateTime Plugins
-        created by Gojecks Joseph
-        can be injected to Project as a required Module
+    @Function Type :  Object Prototype
+    @Return Type : Object
+    
+    Updated October 04,2015 7:10:24 pm
 
-        @Function Type :  Object Prototype
-        @Return Type : Object
-        
-        Updated October 04,2015 7:10:24 pm
+    added a new function format(date)(format)
 
-        added a new function format(date)(format)
+*/
 
-    */
-
-    'use strict';
+'use strict';
 
 
-    var moduleName = 'jeli.date.time',
-        module;
-    try{
-        module = jEli.jModule(moduleName);
-    }catch(e)
-    {
-        module = jEli.jModule(moduleName,{});
-    }
+var moduleName = 'jeli.date.time',
+    module;
+try {
+    module = jEli.jModule(moduleName);
+} catch (e) {
+    module = jEli.jModule(moduleName, {});
+}
 
 module
-.jValue('dateTimeMonthHalf', new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
-.jValue('dateTimeMonthFull', new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'))
-.jValue('dateTimeDayHalf', new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'))
-.jValue('dateTimeDayFull', new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'))
-.jFilter('dateTime',['dateTimeFactory',dateTimeFilterFN])
-.jFilter('timeAgo',['dateTimeFactory',timeAgoFilterFn])
-.jElement('calendar',['dateTimeFactory',calendarFN]);
+    .jValue('dateTimeMonthHalf', new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+    .jValue('dateTimeMonthFull', new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'))
+    .jValue('dateTimeDayHalf', new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'))
+    .jValue('dateTimeDayFull', new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'))
+    .jFilter('dateTime', ['dateTimeFactory', dateTimeFilterFN])
+    .jFilter('timeAgo', ['dateTimeFactory', timeAgoFilterFn])
+    .jElement('calendar', ['dateTimeFactory', calendarFN]);
 
-function dateTimeFilterFN(dateTimeFactory)
-{
-    return function(text,replacer)
-    {
+function dateTimeFilterFN(dateTimeFactory) {
+    return function(text, replacer) {
         return dateTimeFactory.format(dateTimeFactory.$dateTime(text))(replacer);
     }
 }
 
 //timeAgoFilterFn
 //@result converts date to timeago
-function timeAgoFilterFn(dateTimeFactory){
-    return function(text)
-    {
+function timeAgoFilterFn(dateTimeFactory) {
+    return function(text) {
         return dateTimeFactory.$timeConverter(text).timeago;
     }
 }
 
-function calendarFN($dateTimeFactory)
-{
+function calendarFN($dateTimeFactory) {
     var defaultConfig = {
-            fullYear : !1,
-            year : new Date().getFullYear()
-        };
+        fullYear: !1,
+        year: new Date().getFullYear()
+    };
 
     return {
-        allowType : 'AE',
-        $init : function(ele,attr,model)
-        {
-           var config = jEli.$extend( defaultConfig , model.$evaluate(attr.config) );
-                model.calendar = $dateTimeFactory.buildFullCalendar( config );
+        allowType: 'AE',
+        $init: function(ele, attr, model) {
+            var config = jEli.$extend(defaultConfig, model.$evaluate(attr.config));
+            model.calendar = $dateTimeFactory.buildFullCalendar(config);
         }
     }
 }
